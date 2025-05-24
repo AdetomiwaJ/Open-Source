fdb91d2875d8d39d9bfcd98a4a371a4ecde4b132
AdetomiwaJ <42596940+AdetomiwaJ@users.noreply.github.com>
2025-05-24 04:34:46 +0100

    Create appword.py

---
+++ Email_Sender/appword.py
@@ -0,0 +1,4 @@
+my_email = '' #put your email address here ie sender email
+password = '' #Generate app password for gmail https://support.google.com/mail/thread/205453566/how-to-generate-an-app-password?hl=en
+
+#use temp-mail.org to generate a test email
